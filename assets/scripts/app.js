02860e4c79917218e3bda3612ba1828acdb38b69
Erudite885 <erudite885@gmail.com>
2023-01-07 14:51:33 +0100

    if-statements for strong attack functionality

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10;
+const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
@@ -7,18 +8,33 @@ let currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
 
+function attackMonster(attackMode){
+    let maxDamage;
+    if (attackMode === 'ATTACK'){
+        maxDamage = ATTACK_VALUE;
+    } else if (attackMode === 'STRONG_ATTACK'){
+        maxDamage = STRONG_ATTACK_VALUE; 
+    }
+     const damage = dealMonsterDamage(maxDamage);
+     currentMonsterHealth -= damage;
+     const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+     currentPlayerHealth -= playerDamage;
+     if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+       alert("Hurray! Monster destroyed.");
+     } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+       alert("OOPS! You got crushed.");
+     } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
+       alert("emm... There seem to be an entanglement.");
+     }
+}
+
 function attackHandler() {
-  const damage = dealMonsterDamage(ATTACK_VALUE);
-  currentMonsterHealth -= damage;
-  const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
-  currentPlayerHealth -= playerDamage;
-  if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
-    alert("Hurray! Monster destroyed.");
-  } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
-    alert("OOPS! You got crushed.");
-  } else if (currentMonsterHealth <= 0 && currentPlayerHealth <= 0) {
-    alert("emm... There seem to be an entanglement.");
-  }
+    attackMonster('ATTACK');
+}
+
+function strongAttackHandler(){
+    attackMonster("STRONG_ATTACK");
 }
 
 attackBtn.addEventListener("click", attackHandler);
+strongAttackBtn.addEventListener("click", strongAttackHandler);
